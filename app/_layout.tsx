04f88eb5156f86ebfa36bc23bc0b9840c2e3a5e8
dclngbe <dcollin@gbefund.com>
2025-03-07 10:30:36 -0800

    Layout.tsx. Reverting to previous layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,40 @@
-import { Stack } from 'expo-router';
+import { Tabs } from 'expo-router';
 
 export default function RootLayout() {
   return (
-    <Stack
+    <Tabs
       screenOptions={{
         headerShown: false,
-        contentStyle: {
+        tabBarStyle: {
           backgroundColor: '#ffffff',
+          borderTopColor: '#e5e7eb',
+          height: 50,
+          paddingBottom: 0,
+          paddingTop: 0,
         },
+        tabBarItemStyle: {
+          paddingVertical: 2,
+        },
+        tabBarIconStyle: {
+          marginBottom: -4,
+          height: 14,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          lineHeight: 14,
+          marginTop: -4,
+          marginBottom: 2,
+        },
+        tabBarActiveTintColor: '#3b82f6',
+        tabBarInactiveTintColor: '#374151',
       }}
     >
-      <Stack.Screen
+      <Tabs.Screen
         name="index"
         options={{
           title: 'Quiver',
         }}
       />
-    </Stack>
+    </Tabs>
   );
 }
