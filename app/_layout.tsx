b8895c86cea6f92409001f8adefed7a82a04172e
dclngbe <dcollin@gbefund.com>
2025-03-07 10:29:11 -0800

    Changing layout.tsx. Swtiching from tab to stack.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,40 +1,21 @@
-import { Tabs } from 'expo-router';
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
-    <Tabs
+    <Stack
       screenOptions={{
         headerShown: false,
-        tabBarStyle: {
+        contentStyle: {
           backgroundColor: '#ffffff',
-          borderTopColor: '#e5e7eb',
-          height: 50,
-          paddingBottom: 0,
-          paddingTop: 0,
         },
-        tabBarItemStyle: {
-          paddingVertical: 2,
-        },
-        tabBarIconStyle: {
-          marginBottom: -4,
-          height: 14,
-        },
-        tabBarLabelStyle: {
-          fontSize: 12,
-          lineHeight: 14,
-          marginTop: -4,
-          marginBottom: 2,
-        },
-        tabBarActiveTintColor: '#3b82f6',
-        tabBarInactiveTintColor: '#374151',
       }}
     >
-      <Tabs.Screen
+      <Stack.Screen
         name="index"
         options={{
           title: 'Quiver',
         }}
       />
-    </Tabs>
+    </Stack>
   );
 }
